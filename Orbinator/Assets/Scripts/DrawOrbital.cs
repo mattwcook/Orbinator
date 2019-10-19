cdcee2a4e768a3d50a8207c14787f1a30dcaef05
mattwcook <32008216+mattwcook@users.noreply.github.com>
2019-10-19 16:23:58 -0700

    inital Upload

---
+++ Orbinator/Assets/Scripts/DrawOrbital.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawOrbital : MonoBehaviour
+{
+    public float radius;
+    public float inclinationAngle;
+    public Material orbitalMaterial;
+    public float lineThickness;
+    Vector3[] verticeArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localRotation = Quaternion.Euler(inclinationAngle, 0.0f, 0.0f);
+        GetComponent<LineRenderer>().startWidth = lineThickness;
+        GetComponent<LineRenderer>().useWorldSpace = false;
+        GetComponent<Renderer>().material = orbitalMaterial;
+
+        verticeArray = new Vector3[360 + 1];
+        GetComponent<LineRenderer>().positionCount = 360 + 1;
+
+        for (int h =0; h<= 360; h++)
+        {
+            verticeArray[h] = new Vector3(Mathf.Cos(h * Mathf.PI / 180.0f), 0.0f, Mathf.Sin(h * Mathf.PI / 180.0f)) * radius;
+        }
+        GetComponent<LineRenderer>().SetPositions(verticeArray);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Orbinator/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour
+{
+    public string levelToLoad;
+    public Material highlightMaterial;
+    Material originalMaterial;
+    bool mouseOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalMaterial = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouseOver == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Application.LoadLevel(levelToLoad);
+            }
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material = highlightMaterial;
+        mouseOver = true;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material = originalMaterial;
+        mouseOver = false;
+    }
+}
